a96fe588cb8db6d5c30e4b2d866adf449147407e
Olena Cherba <cherbaa14@gmail.com>
2019-05-01 00:55:42 +0300

    Initial commit

---
+++ routes/entcode.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var start = require('start');
+
+    /* $('#btnEncode').on("click",(evt)=>{
+        //router.get('/enc', function(req, res) {
+         window.location.href='http://localhost:63342/steganograf/views/encode.html?_ijt=ehe9kvdltmmv2qmh7cvpnc9cnl';
+         console.log("gotcha!");
+         // res.render('entcode.pug');
+         //});
+     });*/
+module.exports = router;
+
+
+
+
